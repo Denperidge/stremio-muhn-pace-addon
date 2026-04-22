1dd65905483e5c19c60d4f430f2fb0850a5e9f18
Denperidge <cat@denperidge.com>
2026-04-22 16:44:57 +0200

    merged sub.js -> subs.json.js

--- data/subs.json.js
+++ data/subs.json.js
@@ -1,8 +1,31 @@
 // Generate subs.json, containing relevant subtitle links
-import {writeFileSync} from "fs";
+import {writeFileSync, globSync} from "fs";
+import { spawn } from "child_process";
 import { REGEX_LANG, createId } from "../src/shared.js";
 
-const api = await (await fetch("https://api.github.com/repos/one-pace/one-pace-public-subtitles/contents/main/Release/Final%20Subs")).json();
+const SUBTITLE_CACHE = "data/cache/one-pace-public-subtitles";
+
+async function runShell(command, args={}) {
+    console.log("Running " + command)
+    return new Promise((resolve, reject) => {
+        // Thanks to https://stackoverflow.com/a/32872753
+        const instance = spawn(command, Object.assign({shell: true}, args))
+            .on("error", err => {throw err})
+            .on("close", (code, signal) => {
+                resolve()
+            })
+        instance.stdout.setEncoding("utf-8")
+        instance.stdout.on("data", msg => {
+            console.log(msg)
+        })
+    })
+}
+
+await runShell(`git clone https://github.com/one-pace/one-pace-public-subtitles.git ${SUBTITLE_CACHE}`, {shell: true})
+
+await runShell(`git pull`, {cwd: SUBTITLE_CACHE})
+
+const subFiles = globSync(SUBTITLE_CACHE + "/main/Release/Final Subs/*");
 
 const relevantArcs = [
     "Enies Lobby",
@@ -26,18 +49,15 @@ let seasonIndex = 1;
 const out = {};
 
 // Skip until Enies lobby
-const subs = api.filter(result => {
-        const name = result.name;
-        for (let arc of relevantArcs) {
-            if (name.toLowerCase().includes(arc.toLowerCase())) {
-                return true;
-            }
+subFiles.filter(subtitlePath => {
+    for (let arc of relevantArcs) {
+        if (subtitlePath.toLowerCase().includes(arc.toLowerCase())) {
+            return true;
         }
-        return false;
-    }).forEach(result => {
-    console.log(result.name)
-
-    const {season, episode, lang} = result.name.match(REGEX_LANG).groups;
+    }
+    return false;
+}).forEach(subtitlePath => {
+    const {season, episode, lang} = subtitlePath.match(REGEX_LANG).groups;
     
     if (season != lastSeason) {
         seasonIndex++;
@@ -64,10 +84,11 @@ const subs = api.filter(result => {
         out[id] = []
     }
 
+    console.log(subtitlePath.replace("data/cache/one-pace-public-subtitles", ""))
     out[id].push({
         id: id + ":" + langCode,
         lang: langCode,
-        url: result.download_url
+        url: encodeURI("https://raw.githubusercontent.com/one-pace/one-pace-public-subtitles/main" + subtitlePath.replace("data/cache/one-pace-public-subtitles", ""))
     })
 });
 
