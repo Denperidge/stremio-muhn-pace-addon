a26ffa1e8c84c3220e00959b598a58036f34fe3c
Denperidge <cat@denperidge.com>
2026-04-23 17:15:27 +0200

    Refactor into functions & local variables

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
-const {addonBuilder, serveHTTP} = require("stremio-addon-sdk");
+/* ----- IMPORTS ----- */
 const { env } = require("process");
+const { addonBuilder, serveHTTP } = require("stremio-addon-sdk");
 const { REGEX_FILENAME, ID, createId, RELEVANT_ARCS, RELEVANT_ARC_DO_NOT_DETECT } = require("./shared")
 
+const SUBS = require("../data/subs.json");
+const META = require("../data/meta.json");
+
+/* ----- CONSTANTS ----- */
 const IMAGE_BASE_URL = "https://raw.githubusercontent.com/Denperidge/stremio-muhn-pace-addon/main/images/";
 
 const DESCRIPTION = "Supplement the existing One Pace addons with the Muhn Pace fan project!";
@@ -9,79 +14,84 @@ const LOGO = IMAGE_BASE_URL + "logo.png";
 const POSTER = IMAGE_BASE_URL + "poster.jpg";
 const BACKGROUND = IMAGE_BASE_URL + "background.jpg";
 
-const LINKS = require("../data/links.json");
-const SUBS = require("../data/subs.json");
-const META = require("../data/meta.json");
-const GO_TO_ONEPACE = require("../data/onepace.json").go_to;
 
+/* ----- STARTUP DATA LOADING ----- */
 const META_VIDEOS = [];
 const STREAMS = {};
 
-for (const goTo of GO_TO_ONEPACE) {
-    const seasonIndex = goTo.season;
-    
-    for (let i=goTo.start; i<= goTo.end; i++) {
-        const onepaceId = goTo.onepace_id || goTo.onepace_id_prefix + i;
-        const muhnpaceId = createId(seasonIndex, i.toString());
+function loadMuhnPaceData() {
+    console.log("Loading Muhn Pace links...");
+    const LINKS = require("../data/links.json");
 
-        let title;
-        if (Object.keys(META).includes(muhnpaceId)) {
-            title = META[muhnpaceId].title
-        } else {
-            title = `${RELEVANT_ARCS[seasonIndex-1].replace(RELEVANT_ARC_DO_NOT_DETECT, "")} ${i}`;
+    let seasonIndex = 1;
+    let lastSeason = LINKS[0].name.match(REGEX_FILENAME).groups["season"];
+    for (const link of LINKS) {
+        const {name, url} = link;
+        const {season, episode} = name.match(REGEX_FILENAME).groups;
+
+        if (lastSeason != season) {
+            lastSeason = season;
+            seasonIndex++;
         }
-        title = "[One Pace Addon] " + title
+
+        const id = createId(seasonIndex, episode)
+        const title = (seasonIndex == 14 && parseInt(episode) >= 46) ? `Wano ${episode}`: META[id].title;
+
         META_VIDEOS.push({
-            id: muhnpaceId,
+            id: id,
             title: title,
             released: "2010-12-06T05:00:00.000Z",
             season: seasonIndex,
-            episode: i
+            episode: parseInt(episode),
         });
-        STREAMS[muhnpaceId] = { streams: [{
-            name: "fedew04 One Pace Addon",
+        STREAMS[id] = { streams: [{
+            name: "Pixeldrain",
             title: title,
             type: "series",
-            externalUrl: `stremio:///detail/series/pp_onepace/${onepaceId}?autoplay=true`
+            url: url
         }]};
     }
+    console.log("Loaded Muhn Pace links!");
 }
 
-let seasonIndex = 1;
-let lastSeason = LINKS[0].name.match(REGEX_FILENAME).groups["season"];
-for (const link of LINKS) {
-    const {name, url} = link;
-    const {season, episode} = name.match(REGEX_FILENAME).groups;
+function loadOnePaceData() {
+    console.log("Loading needed One Pace links...");
+    const GO_TO_ONEPACE = require("../data/onepace.json").go_to;
+    
+    for (const goTo of GO_TO_ONEPACE) {
+        const seasonIndex = goTo.season;
+        
+        for (let i=goTo.start; i<= goTo.end; i++) {
+            const onepaceId = goTo.onepace_id || goTo.onepace_id_prefix + i;
+            const muhnpaceId = createId(seasonIndex, i.toString());
 
-    console.log("---")
-    console.log(link)
-    console.log(lastSeason)
-    console.log(season)
-
-    if (lastSeason != season) {
-        lastSeason = season;
-        seasonIndex++;
+            let title;
+            if (Object.keys(META).includes(muhnpaceId)) {
+                title = META[muhnpaceId].title
+            } else {
+                title = `${RELEVANT_ARCS[seasonIndex-1].replace(RELEVANT_ARC_DO_NOT_DETECT, "")} ${i}`;
+            }
+            title = "[One Pace Addon] " + title
+            META_VIDEOS.push({
+                id: muhnpaceId,
+                title: title,
+                released: "2010-12-06T05:00:00.000Z",
+                season: seasonIndex,
+                episode: i
+            });
+            STREAMS[muhnpaceId] = { streams: [{
+                name: "fedew04 One Pace Addon",
+                title: title,
+                type: "series",
+                externalUrl: `stremio:///detail/series/pp_onepace/${onepaceId}?autoplay=true`
+            }]};
+        }
     }
-
-    const id = createId(seasonIndex, episode)
-    const title = (seasonIndex == 14 && parseInt(episode) >= 46) ? `Wano ${episode}`: META[id].title;
-
-    META_VIDEOS.push({
-        id: id,
-        title: title,
-        released: "2010-12-06T05:00:00.000Z",
-        season: seasonIndex,
-        episode: parseInt(episode),
-    });
-    STREAMS[id] = { streams: [{
-        name: "Pixeldrain",
-        title: title,
-        type: "series",
-        url: url
-    }]};
+    console.log("Loaded needed One Pace links!");
 }
 
-
+/* ----- STREMIO API/SDK ----- */
+// See https://github.com/Stremio/stremio-addon-sdk/tree/master/docs/
 const builder = new addonBuilder({
     id: ID,
     description: DESCRIPTION,
@@ -106,8 +116,6 @@ const builder = new addonBuilder({
 });
 
 builder.defineCatalogHandler(args => {
-    console.log("catalog")
-    console.log(args)
     if (args.type == "series" && args.id == ID) {
         return Promise.resolve({ metas: [{
             id: ID,
@@ -121,8 +129,6 @@ builder.defineCatalogHandler(args => {
 });
 
 builder.defineMetaHandler(args => {
-    console.log("meta")
-    console.log(args)
     if (args.type == "series", args.id == ID) {
         return Promise.resolve({meta: {
             id: ID,
@@ -136,28 +142,24 @@ builder.defineMetaHandler(args => {
         }})
     }
     return Promise.resolve({})
-    
-})
+});
+
 builder.defineStreamHandler(args => {
-    console.log("stream")
-    console.log(args)
     if (args.type == "series" && args.id.startsWith(ID)) {
-        console.log("that one")
         return Promise.resolve(STREAMS[args.id])
     }
     return Promise.resolve([])
-
 });
 
 builder.defineSubtitlesHandler(args => {
-    console.log("subs")
     if (args.type == "series" && args.id.startsWith(ID)) {
-        console.log(args)
-        console.log(SUBS[args.id])
         return Promise.resolve({subtitles: SUBS[args.id]});
     }
     return Promise.resolve([])
-})
+});
 
+/* ----- LOAD DATA & START SERVER ----- */
+loadMuhnPaceData();
+loadOnePaceData();
 
-serveHTTP(builder.getInterface(), {port: env.PORT || 3636})
+serveHTTP(builder.getInterface(), {port: env.PORT || 3636});
